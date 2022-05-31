b5bb5d60dc82b84aea62ddfd36359bf0879c7efa
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-05-31 17:08:21 +0000

    some updates

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -21,11 +21,19 @@ const SideBar = () => (
   </div>
 );
 
+const CompanyHeader = () => (
+  <div className="channel-list__header">
+      <p className="channel-list__header__text">ChatApp</p>
+  </div>
+)
 
 function ChannelListContainer() {
   return (
     <>
       <SideBar />
+      <div className="channel-list__list__wrapper">
+        <CompanyHeader />
+      </div>
     </>
   )
 }
